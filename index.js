f58350d769f52f916ae3774d4b5011815fd851c0
alex.baran <alex.baran@pandadoc.com>
2020-05-06 11:17:01 +0300

    find text node and symbol func

--- index.js
+++ index.js
@@ -1,68 +1,100 @@
-const fieldNode = document.getElementById('field-f09a4fa2-d45f-46c3-901a-f25f8c30733b');
-const block = fieldNode.parentElement.childNodes[0];
-
-const isIncludedRect = (genRect, rect) => {
-  const correctRect = {
-    top: genRect.top - 5,
-    left: genRect.left - 5,
-    right: genRect.right + 5,
-    bottom: genRect.bottom + 5,
-  };
-
-  return (
-    (correctRect.top <= rect.top) &&
-    (correctRect.left <= rect.left) &&
-    (correctRect.right >= rect.right) &&
-    (correctRect.bottom >= rect.bottom)
-  )
+const isInside = ({ rect, y }) => {
+  return (y >= rect.top) && (y <= rect.bottom);
 }
 
-const getMinRectNode = (nodes) => {
-  let min = nodes[0].getBoundingClientRect().width * nodes[0].getBoundingClientRect().height;
-  let node = nodes[0];
-
-  for (let i = 1; i < nodes.length; i++) {
-    const current = nodes[i].getBoundingClientRect().width * nodes[i].getBoundingClientRect().height;
-    if (current < min) {
-      min = current;
-      node = nodes[i];
-    }
-  }
-
-  return node;
-}
-
-const isNode = (node) => node.nodeType === 1;
-
-const findNearestNodeByCoordinates = (block, field) => {
-  const fieldRect = field.getBoundingClientRect();
-  let firstItemRect;
-  let stack = Array.prototype.slice.call(block.childNodes);
-  const result = [];
-  let item = null;
-
-  while (stack.length) {
-    if (isNode(stack[0])) {
-      firstItemRect = stack[0].getBoundingClientRect();
-    } else {
-      const parentNode = stack[0].parentElement;
-      result.push(parentNode);
-    }
-
-    if (isIncludedRect(firstItemRect, fieldRect)) {
-      item = stack[0];
-
-      if (stack[0].childNodes) {
-        let childNodes = item.childNodes;
-        stack.shift();
-        stack = [...Array.prototype.slice.call(childNodes), ...stack];
-      }
-    } else {
-      stack.shift();
-    }
-  }
-
-  return result.length ? getMinRectNode(result) : item;
+const hasVerticalIntersection = ({ nodeRect, fieldRect }) => {
+  return isInside({ rect: nodeRect, y: fieldRect.top }) || isInside({ rect: nodeRect, y: fieldRect.bottom });
 };
 
-console.log(findNearestNodeByCoordinates(block, fieldNode));
+const getDistance = (pointA, pointB) => {
+  const x = pointA.x - pointB.x;
+  const y = pointA.y - pointB.y;
+  return Math.sqrt(x*x + y*y);  
+};
+
+
+const findTextNodeIntersections = ({ blockNode, fieldNode }) => {
+  const treeWalker = document.createTreeWalker(blockNode, NodeFilter.SHOW_TEXT);
+  const fieldRect = fieldNode.getBoundingClientRect();
+  let currentNode = treeWalker.currentNode;
+
+  const nodes = [];
+  const nodeRange = document.createRange();
+
+  while(currentNode) {
+    if (currentNode.nodeType === Node.TEXT_NODE) {
+      nodeRange.selectNode(currentNode);
+      const nodeRect = nodeRange.getBoundingClientRect();
+
+      if (hasVerticalIntersection({ nodeRect, fieldRect })) {
+        nodes.push(currentNode);
+      }
+    }
+    currentNode = treeWalker.nextNode(); 
+  }
+  nodeRange.detach();
+
+  return nodes;
+};
+
+const getMinDistanceSymbol = ({ textNode, fieldRect }) => {
+  const textNodeRange = document.createRange();
+  const textLength = textNode.textContent.length;
+  const min = {
+    distance: Number.POSITIVE_INFINITY,
+    offset: -1,
+  };
+
+  for (let i = 0; i < textLength - 1; i++) {
+    textNodeRange.setStart(textNode, i);
+    textNodeRange.setEnd(textNode, i + 1);
+
+    const distance = getDistance(fieldRect, textNodeRange.getBoundingClientRect());
+    if (distance < min.distance) {
+      min.distance = distance;
+      min.offset = i;
+    }
+  }
+  textNodeRange.detach();
+
+  return min;
+};
+
+const getNearestTextNodeSymbol = ({ blockNode, fieldNode }) => {
+  if (!blockNode || !fieldNode) {
+    return null;
+  }
+
+  const fieldRect = fieldNode.getBoundingClientRect();
+  const textNodes = findTextNodeIntersections({ blockNode, fieldNode });
+
+  if (textNodes.length) {
+    const min = {
+      distance: Number.POSITIVE_INFINITY,
+      offset: -1,
+      node: null,
+    };
+
+    textNodes.forEach((textNode) => {
+      const { distance, offset } = getMinDistanceSymbol({ textNode, fieldRect });
+      if (distance < min.distance) {
+        min.distance = distance;
+        min.offset = offset;
+        min.node = textNode;
+      }
+    });
+
+    return min;
+  }
+
+  return null;
+};
+
+
+
+const fieldNode = document.getElementById('field-f09a4fa2-d45f-46c3-901a-f25f8c30733b');
+const blockNode = fieldNode.parentElement.childNodes[0];
+
+const min = getNearestTextNodeSymbol({ blockNode, fieldNode });
+
+console.log(min);
